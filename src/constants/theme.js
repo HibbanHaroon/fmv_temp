d2d8acb22f4de816bd669c61966cd0b378ade046
afaqez <afaqkhan113@gmail.com>
2024-07-05 04:25:29 +0500

    Set up Basic Folder Structure

---
+++ src/constants/theme.js
@@ -0,0 +1,25 @@
+// src/theme.js
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  components: {
+    MuiContainer: {
+      styleOverrides: {
+        root: {
+          "&.MuiContainer-maxWidthMd": {
+            maxWidth: "700px",
+          },
+        },
+      },
+    },
+  },
+  palette: {
+    primary: { main: "#FE4747", light: "#FFEBEB" },
+    black: { text: "#222222" },
+    white: "#FFFFFF",
+    green: { text: "#039F8D", light: "#EAF9F7" },
+    grey: { text: "#B0B0B0", border: "#DDDDDD" },
+  },
+});
+
+export default theme;
