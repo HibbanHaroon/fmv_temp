2475c93949f8f5b6dee09f269f80b10489d9a259
HibbanHaroon <hibbanharoon@gmail.com>
2024-07-11 09:02:46 +0500

    Fix Image Size in Marquee

--- src/components/LandingPageMarquee.js
+++ src/components/LandingPageMarquee.js
@@ -5,6 +5,8 @@ import yachtImage from "../assets/images/yacht_image.svg";
 import ballroomImage from "../assets/images/ballroom_image.svg";
 import auditoriumImage from "../assets/images/auditorium_image.svg";
 import museumImage from "../assets/images/museum_image.svg";
+import { useTheme } from "@mui/material/styles";
+import { useMediaQuery } from "@mui/material";
 
 const LandingPageMarquee = () => {
   const items = [
@@ -15,15 +17,33 @@ const LandingPageMarquee = () => {
     { image: museumImage, text: "Museum" },
   ];
 
+  const theme = useTheme();
+  const isXs = useMediaQuery(theme.breakpoints.only("xs"));
+  const isSm = useMediaQuery(theme.breakpoints.only("sm"));
+  const isMd = useMediaQuery(theme.breakpoints.only("md"));
+  const isLg = useMediaQuery(theme.breakpoints.only("lg"));
+
+  const getImageStyle = () => {
+    if (isXs) {
+      return { width: "200px", height: "200px" };
+    }
+    if (isSm) {
+      return { width: "200px", height: "200px" };
+    }
+    if (isMd) {
+      return { width: "300px", height: "300px" };
+    }
+    if (isLg) {
+      return { width: "300px", height: "400px" };
+    }
+    return { width: "300px", height: "400px" }; // Default for larger screens
+  };
+
   return (
     <Marquee velocity={100}>
       {items.map((item, index) => (
         <div key={index} style={{ textAlign: "center", margin: 0, padding: 0 }}>
-          <img
-            src={item.image}
-            alt={item.text}
-            style={{ width: "300px", height: "400px" }}
-          />
+          <img src={item.image} alt={item.text} style={getImageStyle()} />
           <p>{item.text}</p>
         </div>
       ))}
