6a4fc4c272bbc444bb9efac37ef29b0de902bb7e
afaqkhannn <afaqkhan113@gmail.com>
2024-07-15 01:31:55 +0500

    Fix Log In Page & Styling

--- src/pages/onboarding/Login.js
+++ src/pages/onboarding/Login.js
@@ -1,15 +1,19 @@
+// src/pages/onboarding/Login.js
+
 import * as React from "react";
 import { useState } from "react";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 import ResponsiveAppBar from "../../components/ResponsiveAppBar";
+import OutlinedLabelledTextField from "../../components/OutlinedLabelledTextfield";
 import backgroundIcon from "../../assets/images/background_icon.svg";
+import { useTheme } from "@mui/material/styles";
 
 function Login() {
+  const theme = useTheme();
   const [workMail, setWorkMail] = useState("");
   const [createPassword, setCreatePassword] = useState("");
 
@@ -39,70 +43,56 @@ function Login() {
             <Typography
               variant="h4"
               gutterBottom
-              sx={{ mt: 10, fontWeight: "bold" }}
+              sx={{
+                mt: { xs: 7, md: 10 },
+                fontWeight: "bold",
+                fontSize: { xs: "1.6rem" },
+              }}
             >
               Log in as{" "}
-              <span style={{ color: "primary.main" }}>Venue Owner</span>
+              <span style={{ color: theme.palette.primary.main }}>
+                Venue Owner
+              </span>
             </Typography>
             <Typography
-              // variant="h6"
+              variant="body1"
               gutterBottom
-              sx={{ mb: 5, color: "black.text" }}
+              sx={{ mb: { md: 4 }, color: "grey.text" }}
             >
               Welcome back! Log in and continue your journey.
             </Typography>
           </Box>
         </Container>
-        <Container maxWidth="sm">
-          <Box sx={{ mt: 7 }}>
+        <Container
+          maxWidth="sm"
+          sx={{ display: "flex", justifyContent: "center" }}
+        >
+          <Box sx={{ mt: 7, width: { xs: "95%", md: "80%" } }}>
             <form onSubmit={handleSubmit}>
-              <TextField
-                required
-                fullWidth
+              <OutlinedLabelledTextField
                 id="workMail"
+                label="Work Email"
                 placeholder="user@workapp.com"
-                margin="normal"
-                variant="outlined"
                 type="email"
-                label="Work Mail"
                 value={workMail}
                 onChange={(e) => setWorkMail(e.target.value)}
-                InputLabelProps={{ shrink: true }}
-                InputProps={{
-                  style: {
-                    borderRadius: "10px",
-                  },
-                }}
               />
 
-              <TextField
-                required
-                fullWidth
+              <OutlinedLabelledTextField
                 id="createPassword"
-                placeholder="Enter your password"
-                margin="normal"
-                variant="outlined"
-                type="password"
                 label="Password"
+                placeholder="Enter your password"
+                type="password"
                 value={createPassword}
                 onChange={(e) => setCreatePassword(e.target.value)}
-                InputLabelProps={{ shrink: true }}
-                InputProps={{
-                  style: {
-                    borderRadius: "10px",
-                  },
-                }}
               />
 
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "flex-end",
-                  mt: 1,
-                  mb: 6,
-                }}
-              >
-                <Link href="#" color="primary" variant="body2">
+              <Box sx={{ mb: 2, textAlign: "right" }}>
+                <Link
+                  href="#"
+                  color="primary"
+                  sx={{ textDecoration: "none", fontWeight: "500" }}
+                >
                   Forgot Password?
                 </Link>
               </Box>
@@ -113,26 +103,34 @@ function Login() {
                 fullWidth
                 size="large"
                 sx={{
+                  mb: { xs: 1, md: 3 },
                   borderRadius: "8px",
                   mt: 2,
                   backgroundColor: isFormValid()
                     ? "primary.main"
                     : "grey.border",
-                  color: isFormValid() ? "white" : "grey.text",
+                  color: isFormValid() ? "white" : "text",
                 }}
                 disabled={!isFormValid()}
               >
-                Log in
+                Log In
               </Button>
             </form>
 
             <Typography
               variant="body2"
-              sx={{ fontSize: "1.4rem", mt: 2, color: "grey.text " }}
+              sx={{
+                fontSize: "1rem",
+                color: "grey.text",
+              }}
             >
               Don't have an account?{" "}
-              <Link href="#" color="primary">
-                Register.
+              <Link
+                href="#"
+                color="primary"
+                sx={{ textDecoration: "none", fontWeight: "500" }}
+              >
+                Register
               </Link>
             </Typography>
           </Box>
