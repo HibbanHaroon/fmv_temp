ee000f8174e4866559a88a235b01934b06a56629
HibbanHaroon <hibbanharoon@gmail.com>
2024-07-14 16:13:22 +0500

    Modify Multiple Flow to add Restaurant Venue View fixes #6

--- src/pages/onboarding/flows/MultipleFlow.js
+++ src/pages/onboarding/flows/MultipleFlow.js
@@ -7,14 +7,30 @@ import CircularProgressLoader from "../../../components/CircularProgressLoader";
 import RestaurantCard from "../components/RestaurantCard";
 import LabelledRadioGroup from "../components/LabelledRadioGroup";
 import { useTheme } from "@mui/material";
+import VenueCard from "../components/VenueCard";
 
 function MultipleFlow({ activeStep, handleContinue, toggleBackButton }) {
   const theme = useTheme();
   const [partOfGroup, setPartOfGroup] = useState("no");
   const [sameLocation, setSameLocation] = useState("no");
+  const [venueType, setVenueType] = useState("no");
   const [withAlcoholNumber, setWithAlcoholNumber] = useState(0);
   const [withoutAlcoholNumber, setWithoutAlcoholNumber] = useState(0);
+  const [quantityNumber, setQuantityNumber] = useState(0);
   const [selectedChips, setSelectedChips] = useState([]);
+
+  //   States for Rendering different Views for Multiple Flow
+  const [restaurantsView, setRestaurantsView] = useState(false);
+  const [restaurantsVenueView, setRestaurantVenueView] = useState(false);
+  const [venueView, setVenueView] = useState(false);
+  const [kidsVenueView, setKidsVenueView] = useState(false);
+
+  // Need this to display another component between activeStep === 3 and 4 of the normal flow
+  const [showAdditionalStep, setShowAdditionalStep] = useState(true);
+
+  let isRestaurantsSelected = false;
+  let hasOtherSelections = false;
+  let isKidsPlayAreaSelected = false;
 
   const handleWithAlcoholNumberChange = (event) => {
     setWithAlcoholNumber(event.target.value);
@@ -36,10 +52,61 @@ function MultipleFlow({ activeStep, handleContinue, toggleBackButton }) {
     setSelectedChips(chips);
   };
 
-  //   For Restaurant Only flow
-  const isOnlyRestaurantsSelected =
-    selectedChips.length === 1 &&
-    selectedChips[0] === "Restaurants, Cafe's & Bar";
+  const handleQuantityNumberChange = (event) => {
+    setQuantityNumber(event.target.value);
+  };
+
+  const handleVenueTypeChange = (event) => {
+    setVenueType(event.target.value);
+  };
+
+  useEffect(() => {
+    isRestaurantsSelected = selectedChips.includes("Restaurants, Cafe's & Bar");
+    hasOtherSelections = selectedChips.length > 1;
+    isKidsPlayAreaSelected = selectedChips.includes("Kids Play Area");
+
+    // Using these setFunctions as radio buttons, only one true at a time.
+    if (isRestaurantsSelected && !hasOtherSelections) {
+      setRestaurantsView(true);
+      setRestaurantVenueView(false);
+      setVenueView(false);
+      setKidsVenueView(false);
+
+      //   Don't need to display the additional step for restaurants view
+      setShowAdditionalStep(false);
+    } else if (
+      isRestaurantsSelected &&
+      hasOtherSelections &&
+      !isKidsPlayAreaSelected
+    ) {
+      setRestaurantsView(false);
+      setRestaurantVenueView(true);
+      setVenueView(false);
+      setKidsVenueView(false);
+
+      //   Must display the additional step for restaurants view
+      setShowAdditionalStep(true);
+    } else if (
+      !isRestaurantsSelected &&
+      !isKidsPlayAreaSelected &&
+      hasOtherSelections
+    ) {
+      setRestaurantsView(false);
+      setRestaurantVenueView(false);
+      setVenueView(true);
+      setKidsVenueView(false);
+    } else if (isKidsPlayAreaSelected && !hasOtherSelections) {
+      setRestaurantsView(false);
+      setRestaurantVenueView(false);
+      setVenueView(false);
+      setKidsVenueView(true);
+    } else {
+      setRestaurantsView(false);
+      setRestaurantVenueView(false);
+      setVenueView(false);
+      setKidsVenueView(false);
+    }
+  }, [selectedChips]);
 
   //   To hide the Back Button on the Message View
   useEffect(() => {
@@ -49,6 +116,10 @@ function MultipleFlow({ activeStep, handleContinue, toggleBackButton }) {
       toggleBackButton(true);
     }
   }, [activeStep, toggleBackButton]);
+
+  const toggleAdditionalStep = () => {
+    setShowAdditionalStep(!showAdditionalStep);
+  };
 
   return (
     <>
@@ -63,12 +134,12 @@ function MultipleFlow({ activeStep, handleContinue, toggleBackButton }) {
       {activeStep === 3 && (
         <Box sx={{ width: "100%" }}>
           <TitleDescriptionCoupon isCoupon={true} />
-          {/* Remove Same Location Radio Group if Restaurant is selected */}
+          {/* Remove Same Location Radio Group if Only Restaurant is selected */}
           <RestaurantCard
-            isSameLocation={isOnlyRestaurantsSelected ? true : false}
-            sameLocation={isOnlyRestaurantsSelected ? sameLocation : null}
+            isSameLocation={restaurantsView ? true : false}
+            sameLocation={restaurantsView ? sameLocation : null}
             handleSameLocationChange={
-              isOnlyRestaurantsSelected ? handleSameLocationChange : null
+              restaurantsView ? handleSameLocationChange : null
             }
             description={"Cafes, bars, restaurants and food places."}
             withAlcoholNumber={withAlcoholNumber}
@@ -76,32 +147,75 @@ function MultipleFlow({ activeStep, handleContinue, toggleBackButton }) {
             handleWithAlcoholNumberChange={handleWithAlcoholNumberChange}
             handleWithoutAlcoholNumberChange={handleWithoutAlcoholNumberChange}
           />
+          {/* Display Venue Card for Restaurant & Venue View */}
+          {restaurantsVenueView && (
+            <VenueCard
+              isLocation={true}
+              labelText={"Are your venues at the same location?"}
+              sameLocation={sameLocation}
+              handleSameLocationChange={handleSameLocationChange}
+              isOtherText={"true"}
+              otherText={
+                "Choose the total no. of venue available in your property"
+              }
+              toolTipText={
+                "A multi-venue is a complex or facility that houses multiple event spaces within the same location, excluding any areas designated for restaurant services."
+              }
+              quantityNumber={quantityNumber}
+              handleQuantityNumberChange={handleQuantityNumberChange}
+              title={"Venues"}
+              description={"Ballroom, Dance Studio +4"}
+            />
+          )}
         </Box>
       )}
       {activeStep === 4 && (
         <Box sx={{ width: "100%" }}>
           <TitleDescriptionCoupon isCoupon={true} />
-
-          <Card
-            style={{
-              display: "flex",
-              flexDirection: { xs: "column", md: "row" },
-              justifyContent: "space-between",
-              backgroundColor: "white",
-              width: { xs: "80%", md: "90%" },
-              padding: "2rem",
-              marginTop: "2rem",
-              border: `2px solid ${theme.palette.grey.border}`,
-              borderRadius: "12px",
-              boxShadow: "none",
-            }}
-          >
-            <LabelledRadioGroup
-              label={"Are you part of a group?"}
-              value={partOfGroup}
-              handleChange={handlePartOfGroupChange}
-            />
-          </Card>
+          {!restaurantsView && showAdditionalStep && (
+            <Card
+              style={{
+                display: "flex",
+                flexDirection: { xs: "column", md: "row" },
+                justifyContent: "space-between",
+                backgroundColor: "white",
+                width: { xs: "80%", md: "90%" },
+                padding: "2rem",
+                marginTop: "2rem",
+                border: `2px solid ${theme.palette.grey.border}`,
+                borderRadius: "12px",
+                boxShadow: "none",
+              }}
+            >
+              <LabelledRadioGroup
+                label={"Are the venues part establishment complex or building?"}
+                value={venueType}
+                handleChange={handleVenueTypeChange}
+              />
+            </Card>
+          )}
+          {!showAdditionalStep && (
+            <Card
+              style={{
+                display: "flex",
+                flexDirection: { xs: "column", md: "row" },
+                justifyContent: "space-between",
+                backgroundColor: "white",
+                width: { xs: "80%", md: "90%" },
+                padding: "2rem",
+                marginTop: "2rem",
+                border: `2px solid ${theme.palette.grey.border}`,
+                borderRadius: "12px",
+                boxShadow: "none",
+              }}
+            >
+              <LabelledRadioGroup
+                label={"Are you part of a group?"}
+                value={partOfGroup}
+                handleChange={handlePartOfGroupChange}
+              />
+            </Card>
+          )}
         </Box>
       )}
       {activeStep === 5 && (
@@ -123,7 +237,13 @@ function MultipleFlow({ activeStep, handleContinue, toggleBackButton }) {
               boxShadow: "none",
               width: { xs: "100%", md: "70%" },
             }}
-            onClick={handleContinue}
+            onClick={() => {
+              if (activeStep === 4 && showAdditionalStep) {
+                toggleAdditionalStep();
+              } else {
+                handleContinue();
+              }
+            }}
           >
             Continue
           </Button>
